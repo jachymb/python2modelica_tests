64699e019ba61a7356a1436c79f367437ff42f06
Jáchym Barvínek <jachymb@gmail.com>
2014-04-05 15:02:31 +0200

    Code refactoring

--- agformat.py
+++ agformat.py
@@ -1,43 +1,8 @@
 #!/usr/bin/python
 # This script formats the grammar to better readable form and generates list of tokens
-from functools import reduce
-from operator import add
 import sys
 
 SPECIALTOKENS = {"LPAREN": '(', "RPAREN": ')', "COMMA": ",", "LBRACE": '[', "RBRACE": ']̈́'}
-
-GRAMMAR = """mod : Module LPAREN LBRACE listof_stmt RBRACE RPAREN
-stmt : FunctionDef LPAREN string COMMA arguments COMMA LBRACE listof_stmt RBRACE COMMA LBRACE listof_expr RBRACE COMMA optional_expr RPAREN | ClassDef LPAREN string COMMA LBRACE listof_expr RBRACE COMMA LBRACE listof_keyword RBRACE COMMA optional_expr COMMA optional_expr COMMA LBRACE listof_stmt RBRACE COMMA LBRACE listof_expr RBRACE RPAREN | Return LPAREN optional_expr RPAREN | Delete LPAREN LBRACE listof_expr RBRACE RPAREN | Assign LPAREN LBRACE listof_expr RBRACE COMMA expr RPAREN | AugAssign LPAREN expr COMMA arithmeticop COMMA expr RPAREN | For LPAREN expr COMMA expr COMMA LBRACE listof_stmt RBRACE COMMA LBRACE listof_stmt RBRACE RPAREN | While LPAREN expr COMMA LBRACE listof_stmt RBRACE COMMA LBRACE listof_stmt RBRACE RPAREN | If LPAREN expr COMMA LBRACE listof_stmt RBRACE COMMA LBRACE listof_stmt RBRACE RPAREN | With LPAREN LBRACE listof_withitem RBRACE COMMA LBRACE listof_stmt RBRACE RPAREN | Raise LPAREN optional_expr COMMA optional_expr RPAREN | Try LPAREN LBRACE listof_stmt RBRACE COMMA LBRACE listof_excepthandler RBRACE COMMA LBRACE listof_stmt RBRACE COMMA LBRACE listof_stmt RBRACE RPAREN | Assert LPAREN expr COMMA optional_expr RPAREN | Import LPAREN LBRACE listof_alias RBRACE RPAREN | ImportFrom LPAREN optional_identifier COMMA LBRACE listof_alias RBRACE COMMA optional_object RPAREN | Global LPAREN LBRACE listof_identifier RBRACE RPAREN | Nonlocal LPAREN LBRACE listof_identifier RBRACE RPAREN | Expr LPAREN expr RPAREN | Pass LPAREN RPAREN | Break LPAREN RPAREN | Continue LPAREN RPAREN
-expr : BoolOp LPAREN boolop COMMA LBRACE listof_expr RBRACE RPAREN | BinOp LPAREN expr COMMA arithmeticop COMMA expr RPAREN | UnaryOp LPAREN unaryop COMMA expr RPAREN | Lambda LPAREN arguments COMMA expr RPAREN | IfExp LPAREN expr COMMA expr COMMA expr RPAREN | Dict LPAREN LBRACE listof_expr RBRACE COMMA LBRACE listof_expr RBRACE RPAREN | Set LPAREN LBRACE listof_expr RBRACE RPAREN | ListComp LPAREN expr COMMA LBRACE listof_comprehension RBRACE RPAREN | SetComp LPAREN expr COMMA LBRACE listof_comprehension RBRACE RPAREN | DictComp LPAREN expr COMMA expr COMMA LBRACE listof_comprehension RBRACE RPAREN | GeneratorExp LPAREN expr COMMA LBRACE listof_comprehension RBRACE RPAREN | Yield LPAREN optional_expr RPAREN | YieldFrom LPAREN expr RPAREN | Compare LPAREN expr COMMA LBRACE listof_cmpop RBRACE COMMA LBRACE listof_expr RBRACE RPAREN | Call LPAREN expr COMMA LBRACE listof_expr RBRACE COMMA LBRACE listof_keyword RBRACE COMMA optional_expr COMMA optional_expr RPAREN | Num LPAREN object RPAREN | Str LPAREN string RPAREN | Bytes LPAREN b string RPAREN | NameConstant LPAREN singleton RPAREN | Ellipsis LPAREN RPAREN | Attribute LPAREN expr COMMA string COMMA expr_context RPAREN | Subscript LPAREN expr COMMA slice COMMA expr_context RPAREN | Starred LPAREN expr COMMA expr_context RPAREN | Name LPAREN string COMMA expr_context RPAREN | List LPAREN LBRACE listof_expr RBRACE COMMA expr_context RPAREN | Tuple LPAREN LBRACE listof_expr RBRACE COMMA expr_context RPAREN
-expr_context : Load LPAREN RPAREN | Store LPAREN RPAREN | Del LPAREN RPAREN | AugLoad LPAREN RPAREN | AugStore LPAREN RPAREN | Param LPAREN RPAREN
-slice : Slice LPAREN optional_expr COMMA optional_expr COMMA optional_expr RPAREN | ExtSlice LPAREN LBRACE listof_slice RBRACE RPAREN | Index LPAREN expr RPAREN
-boolop : And LPAREN RPAREN | Or LPAREN RPAREN
-arithmeticop : Add LPAREN RPAREN | Sub LPAREN RPAREN | Mult LPAREN RPAREN | Div LPAREN RPAREN | Mod LPAREN RPAREN | Pow LPAREN RPAREN | LShift LPAREN RPAREN | RShift LPAREN RPAREN | BitOr LPAREN RPAREN | BitXor LPAREN RPAREN | BitAnd LPAREN RPAREN | FloorDiv LPAREN RPAREN
-unaryop : Invert LPAREN RPAREN | Not LPAREN RPAREN | UAdd LPAREN RPAREN | USub LPAREN RPAREN
-cmpop : Eq LPAREN RPAREN | NotEq LPAREN RPAREN | Lt LPAREN RPAREN | LtE LPAREN RPAREN | Gt LPAREN RPAREN | GtE LPAREN RPAREN | Is LPAREN RPAREN | IsNot LPAREN RPAREN | In LPAREN RPAREN | NotIn LPAREN RPAREN
-comprehension : comprehension_ LPAREN expr COMMA expr COMMA LBRACE listof_expr RBRACE RPAREN
-excepthandler : ExceptHandler LPAREN optional_expr COMMA optional_identifier COMMA LBRACE listof_stmt RBRACE RPAREN
-arguments : arguments_ LPAREN LBRACE listof_arg RBRACE COMMA optional_arg COMMA LBRACE listof_arg RBRACE COMMA LBRACE listof_expr RBRACE COMMA optional_arg COMMA LBRACE listof_expr RBRACE RPAREN
-arg : arg_ LPAREN string COMMA optional_expr RPAREN
-keyword : keyword_ LPAREN string COMMA expr RPAREN
-alias : alias_ LPAREN string COMMA optional_identifier RPAREN
-withitem : withitem_ LPAREN expr COMMA optional_expr RPAREN
-singleton : True | False | None
-optional_expr : expr | None
-optional_identifier : string | None
-optional_object : object | None
-optional_arg : arg | None
-listof_stmt : stmt COMMA listof_stmt | stmt |
-listof_expr : expr COMMA listof_expr | expr |
-listof_keyword : keyword COMMA listof_keyword | keyword |
-listof_withitem : withitem COMMA listof_withitem | withitem |
-listof_excepthandler : excepthandler COMMA listof_excepthandler | excepthandler |
-listof_alias : alias COMMA listof_alias | alias |
-listof_identifier : string COMMA listof_identifier | string |
-listof_comprehension : comprehension COMMA listof_comprehension | comprehension |
-listof_cmpop : cmpop COMMA listof_cmpop | cmpop |
-listof_slice : slice COMMA listof_slice | slice |
-listof_arg : arg COMMA listof_arg | arg |"""
 
 GRAMMAR_PYTHON = \
 [('mod', [
@@ -209,6 +174,8 @@ GRAMMAR_PYTHON = \
             ['arg'], 
             []])]
 
+OPERATORTYPES =  {"arithmeticop", "cmpop", "unaryop", "boolop"}
+TYPETOKENS = {"object", "string"}
 
 ARGNAMES = {
     'Add': (),
@@ -287,7 +254,7 @@ ARGNAMES = {
     'RShift': (),
     'SetComp': ('elt', 'generators'),
     'Set': ('elts',),
-    "singleton": ()}
+    'singleton': (),
     'Slice': ('lower', 'upper', 'step'),
     'Starred': ('value', 'ctx'),
     'Store': (),
@@ -303,27 +270,57 @@ ARGNAMES = {
     'withitem_': ('context_expr', 'optional_vars'),
     'With': ('items', 'body'),
     'YieldFrom': ('value',),
-    'Yield': ('value',),
-#dict([ (l[:l.find("(")], tuple(map(lambda x: x.split(" ")[1] , l[l.find("(")+1:-2].split(", ") if l.find("(") != -1 else []) )) for l in open("fullgrammar.txt").read().splitlines()])
+    'Yield': ('value',)}
 
-START = "mod"
+START = "mod" # GRAMMAR_PYTHON[0][0]
+
+def flatten(l):
+    """ Makes recursively nested list flat"""
+    r = []
+    for e in l:
+        if type(e) != list:
+            r.append(e)
+        else:
+            r.extend(flatten(e))
+    return r
+
 
 def converttype(s):
+    """ Used for converting tokens which represent constant values 
+    from Python AST to Modelica AST types. Also for converting types
+    representing lists and options"""
     if s == "object": return "String"
     elif s == "string": return "String"
-    elif s == "identifier": return "String"
-    else: return s
-
-rules = [l.split(":",1) for l in GRAMMAR.splitlines()]
-rules = [(l[0].strip(), list(map(str.strip, l[1].split("|")))) for l in rules]
-
-heads, prods =  tuple(zip(*rules))
-indentsize = max(map(len, heads)) + 1
+    elif islistof(s): return "list<%s>" % converttype(s[7:])
+    elif isoptional(s): return "Option<%s>" % converttype(s[9:])
+    else: return s # No change - general token
    
-def formatToken(head, t):
+def tokenHeads(t):
+    """ Returns all nonterminals which have the given token directly
+    in their production """
+    return { GRAMMAR_PYTHON[x][0] for x in range(len(GRAMMAR_PYTHON))
+               if t in flatten(GRAMMAR_PYTHON[x][1]) }
+
+def formatToken(t, constructor = False):
+    """ Returns the proper format of token which should appear in parser
+    and consequently also in lexer return values."""
     # Assuming t is either terminal or non-terminal of grammar
+    # Do not format non-termials
     if t in heads: return t
-    else: return t.upper().replace("_","") 
+
+    if constructor and t == "None": return "PYNONE" # To not confuse with Option "NONE"
+
+    # We rename operator tokens to avoid conflicts with Modelica operators
+    if t not in SPECIALTOKENS and tokenHeads(t).intersection(OPERATORTYPES) != set():
+        t += "_OP"
+    # Trailing underscore is not needed in formated tokens
+    elif t.endswith("_"): t = t[:-1]
+    return t.upper()
+
+def islistof(x):
+    return x.startswith("listof_")
+def isoptional(x):
+    return x.startswith("optional_")
 
 if __name__ == "__main__":
     if len(sys.argv) != 3:
@@ -331,66 +328,50 @@ if __name__ == "__main__":
         sys.exit()
 
 
+    heads, prods =  tuple(zip(*GRAMMAR_PYTHON))
+    indentsize = max(map(len, heads)) + 1 # Indent size in the bison file for better readability
 
-    nottokens = set(heads) #.union({"'('", "')'", "'['", "']'", "','", "/*", "*/", "empty", "'b'"})
-    # Also do not print tokens for data types
-    nottokens = nottokens.union({"string", "object"})
-    nottokens = nottokens.union({""})
+    nottokens = set(heads) 
+    nottokens = nottokens.union(TYPETOKENS) # We need to translate TYPETOKENS values to Modelica AST.
 
-    tokens = set(reduce(add, [p.split(" ") for p in reduce(add, prods)])).difference(nottokens)
-    #with open(sys.argv[1], "w") as tf:
-    #    print("\n".join(sorted(tokens)), file=tf)
+    tokens = set(flatten(prods)).difference(nottokens)
 
 
     # Print the head of the parser
     print("%{\nimport Absyn;\ntype AstTree = Absyn."+START+";\n")
     print("\n".join([ "type %s = %s;" % z for z in
-            ( (x, ("list<%s>" % x[7:] if x.startswith("listof_") else
-                  ("Option<%s>" % x[9:] if x.startswith("optional_") else
-                  ("Absyn."+x))))
+            ( (x, (converttype(x) if islistof(x) or isoptional(x) else
+                  ("Absyn."+x)))
                 for x in heads if x != START)]))
     print("\n%}\n")
 
-    # Print all tokens
-    print("%token STRING\n%token OBJECT\n")
-    print("\n".join(["%token "+t.upper() for t in SPECIALTOKENS]))
-    print()
-    print("\n".join(["%token "+t.upper().replace("_","") for t in sorted(tokens) if t not in SPECIALTOKENS] ))
+    # Print all tokens to the parser
+
+    for tokset in (TYPETOKENS, SPECIALTOKENS, tokens.difference(SPECIALTOKENS)):
+        print("\n".join(["%token "+t for t in sorted(map(formatToken, tokset))] ))
+        print()
     print("\n%%\n")
     
     
     with open(sys.argv[2], "w") as aofile:
-        for head, productions in rules:
+        for head, productions in GRAMMAR_PYTHON:
             def print_a(s):
-                if not (head.startswith("listof_") or head.startswith("optional_")):
+                if not (islistof(head) or isoptional(head)):
                     print(s, file=aofile)
 
             print_a("public uniontype "+head)
-            for pn,production in enumerate(productions):
+            for pn,prods in enumerate(productions):
 
-                prods = production.split(" ")
-                constructor = prods[0]
-                recordname = formatToken(head, constructor) #constructor.upper().replace("_", "")
-                if recordname == "NONE": recordname = "PYNONE" # To not confuse with Option "NONE"
-
-
-                # Make records without params oneliners
-                #if len(tuple(filter(lambda x: x not in tokens, prods))) == 0 and \
-                #        not (head.startswith("listof_") or head.startswith("optional_")):
-                #    print_a("\trecord "+recordname+" end "+recordname+";")
-                #    continue
-
-                print_a("\trecord "+recordname)
-                i = 0
                 constructorparams = []
-                if head.startswith("optional_"):
-                    semanticRule = "$$[Option<"+converttype(head[9:])+">] = %s;"
+
+                if isoptional(head): # Lists
+                    semanticRule = "$$[" + converttype(head) + "] = %s;"
                     if pn == 0:
                         semanticRule %= "SOME($1)"
                     else:
                         semanticRule %= "NONE()"
-                elif head.startswith("listof_"):
-                    semanticRule = "$$[list<"+converttype(head[7:])+">] = %s;"
+                elif islistof(head): # Options
+                    semanticRule = "$$[" + converttype(head) + "] = %s;"
                     if pn == 0:
                         semanticRule %= "$1[%s]::$3[%s]" % (converttype(head[7:]), head)
                     elif pn == 1:
@@ -398,8 +379,15 @@ if __name__ == "__main__":
                     else:
                         semanticRule %= "{}"
 
-                else:
+                else: # Ordinary attributes
+
+                    constructor = prods[0]
+                    recordname = formatToken(constructor, True) 
                     semanticRule = "$$["+head+"] = Absyn."+recordname+"(%s);"
+                    
+                    print_a("\trecord "+recordname)
+                    i = 0
+
                     for pi, x in enumerate(prods):
                         if x in tokens: continue
                         argname, i = ARGNAMES[constructor][i], i+1
@@ -412,7 +400,6 @@ if __name__ == "__main__":
                             print_a("\t\t%s\t%s;" % (converttype(x), argname))
                         constructorparams.append("$"+str(pi+1)+"["+x+"]")
                     print_a("\tend "+recordname+";")
-                    i+=1
 
                     semanticRule %= ", ".join(constructorparams)
 
@@ -420,7 +407,7 @@ if __name__ == "__main__":
                 if head == START:
                     semanticRule = "(absyntree)" + semanticRule[2:]
 
-                formatedRule = " ".join([formatToken(head, p) for p in prods])
+                formatedRule = " ".join([formatToken(p) for p in prods])
                 if pn == 0:
                     print("%s: %s\n%s  { %s }" % 
                         (head.ljust(indentsize), formatedRule, " "*indentsize, semanticRule))
